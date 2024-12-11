9d21f25ceaede2e8daff1d7413b29c614b7710c5
Florencia Doval <florencia_doval@brown.edu>
2024-12-11 15:32:10 -0500

    page

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+  const isProduction = mode === "production";
+
   return {
     build: {
       outDir: "build",
@@ -13,23 +15,20 @@ export default defineConfig(({ mode }) => {
       "process.env.AUTH_DOMAIN": JSON.stringify(env.AUTH_DOMAIN),
       "process.env.PROJECT_ID": JSON.stringify(env.PROJECT_ID),
       "process.env.STORAGE_BUCKET": JSON.stringify(env.STORAGE_BUCKET),
-      "process.env.MESSAGING_SENDER_ID": JSON.stringify(
-        env.MESSAGING_SENDER_ID
-      ),
+      "process.env.MESSAGING_SENDER_ID": JSON.stringify(env.MESSAGING_SENDER_ID),
       "process.env.APP_ID": JSON.stringify(env.APP_ID),
       "process.env.MEASUREMENT_ID": JSON.stringify(env.MEASUREMENT_ID),
       "process.env.MAPBOX_TOKEN": JSON.stringify(env.MAPBOX_TOKEN),
-      "process.env.VITE_CLERK_PUBLISHABLE_KEY": JSON.stringify(
-        env.VITE_CLERK_PUBLISHABLE_KEY
-      ),
+      "process.env.VITE_CLERK_PUBLISHABLE_KEY": JSON.stringify(env.VITE_CLERK_PUBLISHABLE_KEY),
     },
     plugins: [react()],
+    base: isProduction ? "/languageGO/" : "/", // Dynamically set base for GitHub Pages
     server: {
       port: 8000,
     },
     css: {
       modules: {
-        generateScopedName: "[name]__[local]___[hash:base64:5]", // Custom class naming pattern
+        generateScopedName: "[name]__[local]___[hash:base64:5]",
       },
     },
     test: {
